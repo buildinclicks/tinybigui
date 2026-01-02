d030b53f5685325449ff44cdb2503652d5fe4c09
pukhrajp <pukhraj.prajapat@gmail.com>
2026-01-02 17:48:48 +0530

    feat(fab): implement material design 3 fab component with 51 passing tests

---
+++ packages/react/src/components/FAB/index.ts
@@ -0,0 +1,6 @@
+export { FAB } from "./FAB";
+export { FABHeadless } from "./FABHeadless";
+export { fabVariants } from "./FAB.variants";
+export type { FABProps, FABSize, FABColor } from "./FAB.types";
+export type { FABVariants } from "./FAB.variants";
+export type { FABHeadlessProps } from "./FABHeadless";
